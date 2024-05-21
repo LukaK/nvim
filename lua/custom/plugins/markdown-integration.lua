c60e21d6dac5c98f0ff26c9e085bab135987e3c3
Luka Krapic <luka.krapic@gmail.com>
2024-05-21 20:17:20 +0200

    feat: start adding markdown plugins

--- lua/custom/plugins/markdown-integration.lua
+++ lua/custom/plugins/markdown-integration.lua
@@ -1,1 +1,19 @@
-return {}
+return {
+  -- {
+  --   'denstiny/styledoc.nvim',
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'vhyrro/luarocks.nvim',
+  --     '3rd/image.nvim',
+  --   },
+  --   opts = true,
+  --   ft = 'markdown',
+  -- },
+  {
+    'plasticboy/vim-markdown',
+    ft = 'markdown',
+    config = function()
+      vim.g.vim_markdown_folding_disabled = 1
+    end,
+  },
+}
