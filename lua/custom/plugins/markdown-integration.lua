c8f7b4be4a26f5a8543799f048e3dca531d90007
Luka Krapic <luka.krapic@gmail.com>
2024-05-22 11:46:34 +0200

    feat: update markdown plugins feat: add vim-matchup

--- lua/custom/plugins/markdown-integration.lua
+++ lua/custom/plugins/markdown-integration.lua
@@ -11,9 +11,20 @@ return {
   -- },
   {
     'plasticboy/vim-markdown',
+    dependencies = {
+      'godlygeek/tabular',
+    },
     ft = 'markdown',
     config = function()
       vim.g.vim_markdown_folding_disabled = 1
+    end,
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
     end,
   },
 }
