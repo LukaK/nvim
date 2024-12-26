a73dfae45c8aab53218e7c04719c2f73c605e61d
Luka Krapic <luka.krapic@gmail.com>
2024-12-26 15:08:24 +0100

    fix: schemastore configuration for yamlls lsp

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -214,11 +214,31 @@ return {
         },
       },
 
+      -- TODO: Customize this
       yamlls = {
         filetypes = { 'yaml', 'yml' },
         settings = {
           yaml = {
-            schemas = require('schemastore').yaml.schemas(),
+            schemaStore = {
+              -- You must disable built-in schemaStore support if you want to use
+              -- schemastore plugin and its advanced options like `ignore`.
+              enable = false,
+              url = '',
+            },
+            schemas = require('schemastore').yaml.schemas {
+
+              -- TODO: Select explicitly or define without schemastore
+              -- select = {
+              --   'kustomization.yaml',
+              --   'docker-compose.yml',
+              -- },
+
+              extra = {
+                url = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json',
+                name = 'Argo CD Application',
+                fileMatch = 'argocd-application.yaml',
+              },
+            },
             customTags = {
               '!And scalar',
               '!And map',
