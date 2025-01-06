dc51f24ebb8a3217d475f781df4afc1b0dfd6d0b
Luka Krapic <luka.krapic@gmail.com>
2025-01-06 23:40:12 +0100

    feat: add icons to mini statusline feat: add schema information to yaml files to mini statusline

--- lua/core/plugins/yaml-companion.lua
+++ lua/core/plugins/yaml-companion.lua
@@ -8,5 +8,14 @@ return {
   config = function()
     require('telescope').load_extension 'yaml_schema'
     vim.keymap.set('n', '<leader>ss', require('yaml-companion').open_ui_select, { desc = '[S]earch [S]chema' })
+
+    -- add schema information instead of section location
+    require('mini.statusline').section_location = function()
+      local schema = require('yaml-companion').get_buf_schema(0)
+      if schema.result[1].name == 'none' then
+        return ''
+      end
+      return schema.result[1].name
+    end
   end,
 }
