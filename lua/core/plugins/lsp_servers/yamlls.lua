1b839e1fe8e626c041f0efa25e6293b6197ab5cd
Luka Krapic <luka.krapic@gmail.com>
2025-01-06 16:56:09 +0100

    feat: add yaml companion for kubernetes schemas feat: update yamlls configuration

--- lua/core/plugins/lsp_servers/yamlls.lua
+++ lua/core/plugins/lsp_servers/yamlls.lua
@@ -1,139 +1,151 @@
-return {
-  filetypes = { 'yaml', 'yml' },
+return require('yaml-companion').setup {
 
-  settings = {
+  builtin_matchers = {
+    -- detect kubernetes files based on content
+    kubernetes = { enabled = true },
+    cloud_init = { enabled = true },
+  },
 
-    yaml = {
+  schemas = {},
 
-      -- disable built-in schemaStore support ( needed by schemastore plugin )
-      schemaStore = {
-        enable = false,
-        url = '',
-      },
+  lspconfig = {
 
-      -- select schemas from schemastore
-      schemas = require('schemastore').yaml.schemas {
+    filetypes = { 'yaml', 'yml' },
 
-        -- manually select schemas from schemastore if file triggers are ok
-        -- see: https://github.com/b0o/SchemaStore.nvim/blob/main/lua/schemastore/catalog.lua
-        select = {
+    settings = {
 
-          -- github workflows
-          'GitHub Workflow',
-          'GitHub Workflow Template Properties',
+      yaml = {
 
-          -- docker
-          'docker-compose.yml', -- file types: docker-compose(yaml|yml), compose.*.(yaml|yml)
-
-          -- kubernetes
-          'kustomization.yaml', -- file types: kustomization.(yaml|yml)
-          'helmfile',
-
-          -- ansible
-          'Ansible Vars File',
-          'Ansible Tasks File',
-          'Ansible Playbook',
-          'Ansible Requirements', -- redefined below
-
-          -- aws
-          'AWS CloudFormation',
-          'AWS SAM CLI Samconfig',
-          'AWS CloudFormation Serverless Application Model (SAM)', -- redefined below
-
-          -- tools
-          'yamllint', -- file types: .yamllint, **/.yamllint.(yaml,yml)
+        -- disable built-in schemaStore support ( needed by schemastore plugin )
+        schemaStore = {
+          enable = false,
+          url = '',
         },
 
-        replace = {
-          -- aws
-          ['AWS CloudFormation Serverless Application Model (SAM)'] = {
-            url = 'https://raw.githubusercontent.com/aws/serverless-application-model/main/samtranslator/schema/schema.json',
-            name = 'AWS CloudFormation Serverless Application Model (SAM)',
-            description = 'AWS SAM schema',
-            fileMatch = {
-              'template.yaml',
-              'serverless.template',
-              '*.sam.json',
-              '*.sam.yml',
-              '*.sam.yaml',
-              'sam.json',
-              'sam.yml',
-              'sam.yaml',
-              'serverless.yaml',
-              'serverless.yml',
+        -- select schemas from schemastore
+        schemas = require('schemastore').yaml.schemas {
+
+          -- manually select schemas from schemastore if file triggers are ok
+          -- see: https://github.com/b0o/SchemaStore.nvim/blob/main/lua/schemastore/catalog.lua
+          select = {
+
+            -- github workflows
+            'GitHub Workflow',
+            'GitHub Workflow Template Properties',
+
+            -- docker
+            'docker-compose.yml', -- file types: docker-compose(yaml|yml), compose.*.(yaml|yml)
+
+            -- kubernetes
+            'kustomization.yaml', -- file types: kustomization.(yaml|yml)
+            'helmfile',
+
+            -- ansible
+            'Ansible Vars File',
+            'Ansible Tasks File',
+            'Ansible Playbook',
+            'Ansible Requirements', -- redefined below
+
+            -- aws
+            'AWS CloudFormation',
+            'AWS SAM CLI Samconfig',
+            'AWS CloudFormation Serverless Application Model (SAM)', -- redefined below
+
+            -- tools
+            'yamllint', -- file types: .yamllint, **/.yamllint.(yaml,yml)
+          },
+
+          replace = {
+            -- aws
+            ['AWS CloudFormation Serverless Application Model (SAM)'] = {
+              url = 'https://raw.githubusercontent.com/aws/serverless-application-model/main/samtranslator/schema/schema.json',
+              name = 'AWS CloudFormation Serverless Application Model (SAM)',
+              description = 'AWS SAM schema',
+              fileMatch = {
+                'template.yaml',
+                'serverless.template',
+                '*.sam.json',
+                '*.sam.yml',
+                '*.sam.yaml',
+                'sam.json',
+                'sam.yml',
+                'sam.yaml',
+                'serverless.yaml',
+                'serverless.yml',
+              },
+            },
+
+            -- ansible
+            ['Ansible Requirements'] = {
+              url = 'https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/requirements.json',
+              name = 'Ansible Requirements',
+              description = 'Ansible requirements file schema',
+              fileMatch = { 'requirements.yml', 'requirements.yaml' },
             },
           },
 
-          -- ansible
-          ['Ansible Requirements'] = {
-            url = 'https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/requirements.json',
-            name = 'Ansible Requirements',
-            description = 'Ansible requirements file schema',
-            fileMatch = { 'requirements.yml', 'requirements.yaml' },
+          -- additional schemas not in the schemastore
+          extra = {
+            {
+              url = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json',
+              name = 'Argo CD Application',
+              fileMatch = 'argocd-application.yaml',
+              description = 'ArgoCd applications schema',
+            },
           },
         },
 
-        -- additional schemas not in the schemastore
-        extra = {
-          {
-            url = 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json',
-            name = 'Argo CD Application',
-            fileMatch = 'argocd-application.yaml',
-            description = 'ArgoCd applications schema',
-          },
+        -- custom tags for aws cloudformation
+        customTags = {
+          '!And scalar',
+          '!And map',
+          '!And sequence',
+          '!If scalar',
+          '!If map',
+          '!If sequence',
+          '!Not scalar',
+          '!Not map',
+          '!Not sequence',
+          '!Equals scalar',
+          '!Equals map',
+          '!Equals sequence',
+          '!Or scalar',
+          '!Or map',
+          '!Or sequence',
+          '!FindInMap scalar',
+          '!FindInMap mappping',
+          '!FindInMap sequence',
+          '!Base64 scalar',
+          '!Base64 map',
+          '!Base64 sequence',
+          '!Cidr scalar',
+          '!Cidr map',
+          '!Cidr sequence',
+          '!Ref scalar',
+          '!Ref map',
+          '!Ref sequence',
+          '!Sub scalar',
+          '!Sub map',
+          '!Sub sequence',
+          '!GetAtt scalar',
+          '!GetAtt map',
+          '!GetAtt sequence',
+          '!GetAZs scalar',
+          '!GetAZs map',
+          '!GetAZs sequence',
+          '!ImportValue scalar',
+          '!ImportValue map',
+          '!ImportValue sequence',
+          '!Select scalar',
+          '!Select map',
+          '!Select sequence',
+          '!Split scalar',
+          '!Split map',
+          '!Split sequence',
+          '!Join scalar',
+          '!Join map',
+          '!Join sequence',
         },
-      },
-
-      -- custom tags for aws cloudformation
-      customTags = {
-        '!And scalar',
-        '!And map',
-        '!And sequence',
-        '!If scalar',
-        '!If map',
-        '!If sequence',
-        '!Not scalar',
-        '!Not map',
-        '!Not sequence',
-        '!Equals scalar',
-        '!Equals map',
-        '!Equals sequence',
-        '!Or scalar',
-        '!Or map',
-        '!Or sequence',
-        '!FindInMap scalar',
-        '!FindInMap mappping',
-        '!FindInMap sequence',
-        '!Base64 scalar',
-        '!Base64 map',
-        '!Base64 sequence',
-        '!Cidr scalar',
-        '!Cidr map',
-        '!Cidr sequence',
-        '!Ref scalar',
-        '!Ref map',
-        '!Ref sequence',
-        '!Sub scalar',
-        '!Sub map',
-        '!Sub sequence',
-        '!GetAtt scalar',
-        '!GetAtt map',
-        '!GetAtt sequence',
-        '!GetAZs scalar',
-        '!GetAZs map',
-        '!GetAZs sequence',
-        '!ImportValue scalar',
-        '!ImportValue map',
-        '!ImportValue sequence',
-        '!Select scalar',
-        '!Select map',
-        '!Select sequence',
-        '!Split scalar',
-        '!Split map',
-        '!Split sequence',
-        '!Join scalar',
-        '!Join map',
-        '!Join sequence',
       },
     },
   },
---
+++ lua/core/plugins/yaml-companion.lua
@@ -0,0 +1,8 @@
+return {
+  'someone-stole-my-name/yaml-companion.nvim',
+  requires = {
+    { 'neovim/nvim-lspconfig' },
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-telescope/telescope.nvim' },
+  },
+}
