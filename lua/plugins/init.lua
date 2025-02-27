f6c986e3f1ac8f9e49e68cc2d5b61707c7193442
Luka Krapic <luka.krapic@gmail.com>
2025-02-27 08:00:22 +0100

    feat: move one line plugins to init feat: cleanup which key configuration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,6 +2,13 @@ return {
 
   -- detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+  'andymass/vim-matchup',
+
+  -- highlight urls
+  'itchyny/vim-highlighturl',
+
+  -- better repeat of commands with .
+  'tpope/vim-repeat',
 
   -- TODO: Remove if mini plugin is working ok
   -- comment lines
