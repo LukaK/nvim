c58062db74e4d2fb0d429bce4c0b2d427ad37965
Luka Krapic <luka.krapic@gmail.com>
2025-04-22 08:40:25 +0200

    feat: add initial commit for llm plugin

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -1,23 +1,23 @@
 -- Large language model support
--- chatgpt url for api keys: https://auth.openai.com/log-in
+-- chatgpt usage details: https://platform.openai.com/usage
+-- chatgpt settings: https://platform.openai.com/settings/organization/billing/payment-methods
 --
 return {
-  -- Lazy
-  -- {
-  --   "jackMort/ChatGPT.nvim",
-  --     event = "VeryLazy",
-  --     config = function()
-  --       require("chatgpt").setup({
-  --         openai_params = {
-  --           model = "gpt-4o",
-  --         }
-  --       })
-  --     end,
-  --     dependencies = {
-  --       "MunifTanjim/nui.nvim",
-  --       "nvim-lua/plenary.nvim",
-  --       "folke/trouble.nvim", -- optional
-  --       "nvim-telescope/telescope.nvim"
-  --     }
-  -- }
+  {
+    "jackMort/ChatGPT.nvim",
+      event = "VeryLazy",
+      config = function()
+        require("chatgpt").setup({
+          openai_params = {
+            model = "gpt-4o",
+          }
+        })
+      end,
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "folke/trouble.nvim", -- optional
+        "nvim-telescope/telescope.nvim"
+      }
+  }
 }
