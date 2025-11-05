51eb6a5634394ebee2985b54b4f53f34ce4281c5
Luka Krapic <luka.krapic@gmail.com>
2025-11-05 09:21:09 +0100

    feat: add noice configuration feat: comment out notifications

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -11,7 +11,7 @@ return {
     scope = { enabled = true },
     bigfile = { enabled = true },
     input = { enabled = true },
-    notifier = { enabled = true },
+    notifier = { enabled = false },
     quickfile = { enabled = true },
     scroll = { enabled = true },
     words = { enabled = true },
